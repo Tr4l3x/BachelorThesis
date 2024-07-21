0a2e87ee6898d89acf9ab9c2c9a3ee92fad2a294
Lukas <lukas.dankwart@gmx.de>
2024-07-21 15:59:57 +0100

    Training Variant with Sample dependencies added

--- process.py
+++ process.py
@@ -4,6 +4,8 @@ import forward_process
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+import numpy as np
+from torch.optim import Adam
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -76,29 +78,86 @@ def sample__plot_image():
             plt.subplot(1, num_images, int(i/stepsize+1))
     plt.show()
 
-
 """
-   Training 
+   Initializing model for later training variants
 """
-from torch.optim import Adam
-
-dataloader = dataset.get_dataloader()
 model = network.SimpleUNet().to(device)
-optimizer = Adam(model.parameters(), lr=0.001)
-batch_size = 32
-epochs = 2
 
-for epoch in range(epochs):
-    for step, batch in enumerate(dataloader):
-        optimizer.zero_grad()
+"""
+   Training variant 0 for testing 
+"""
 
-        t = torch.randint(0, T, (batch.shape[0],), device=device).long()
-        loss = get_loss(model, batch.float(), t)
-        loss.backward()
-        optimizer.step()
+def train_variant0(model):
+    dataloader = dataset.get_dataloader()
+    optimizer = Adam(model.parameters(), lr=0.001)
+    batch_size = 32
+    epochs = 2
 
-        if epoch % 5 == 0 and step == 100:
-            print(f"Epoch {epoch} | step {step:03d} Loss: {loss.item()} ")
-            sample__plot_image()
-    print("EPOCHE VORBEIIII")
-    exit()
+    for epoch in range(epochs):
+        for step, batch in enumerate(dataloader):
+            optimizer.zero_grad()
+
+            t = torch.randint(0, T, (batch.shape[0],), device=device).long()
+
+            loss = get_loss(model, batch.float(), t)
+            loss.backward()
+            optimizer.step()
+
+            if epoch % 5 == 0 and step == 100:
+                print(f"Epoch {epoch} | step {step:03d} Loss: {loss.item()} ")
+                sample__plot_image()
+        print("EPOCHE VORBEIIII")
+        exit()
+
+"""
+   Training variant 1 - with sample dependencies
+"""
+def train_variant1(model):
+    # Dataset object
+    data = dataset.get_dataset()
+    # Holds for each sample a list with all their piano rolls (bars)
+    samples = data.piano_rolls
+
+    # Hyperparameters
+    epochs = 1
+    optimizer = Adam(model.parameters(), lr=0.001)
+    slide_window_size = 2
+
+    for epoch in range(epochs):
+        for sample_idx, sample in enumerate(samples):
+            loss_sum = 0
+            for bar_num, roll in enumerate(sample, start=1):
+                optimizer.zero_grad()
+
+                prev_bars = []
+                # Case if there are not enough previous bars
+                if slide_window_size > bar_num:
+                    for i in range(slide_window_size-bar_num):
+                        prev_bars.append(torch.tensor(np.zeros((72,48))))
+                # Adding existing previous bars
+                window_idx = max(0, bar_num-slide_window_size)
+                for window_idx in range(window_idx, bar_num):
+                    prev_bars.append(sample[window_idx])
+
+                # Concatenating all prev. (or empty) bars with the current bar_num roll
+                batch = torch.cat(prev_bars, dim=1)
+                # Since training_batches should have shape (batch_size, num_of_channels, sample_dim0, sample_dim1)
+                # Adding channel dimension at dim=0 (1, 72, 48*window_size)
+                batch = batch.unsqueeze(0)
+                # Adding channel dimension at dim=1 (1, 1, 72, 48*window_size)
+                batch = batch.unsqueeze(0)
+
+                t = torch.randint(0, T, (batch.shape[0],), device=device).long()
+                loss = get_loss(model, batch.float(), t)
+                loss.backward()
+                optimizer.step()
+
+                loss_sum += loss.item()
+
+            avg_loss = loss_sum / len(sample)
+            print(f"In Epoche {epoch} fuer Sample {sample_idx} | Avg. Loss: {avg_loss} ")
+            if sample_idx == 0:
+                sample__plot_image()
+                exit()
+
+train_variant1(model)
